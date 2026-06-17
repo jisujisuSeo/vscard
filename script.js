a7daed37fa326a8a04542c7474ef957a90e2762c
SEO JISU <seogs1004@naver.com>
2026-06-18 01:01:40 +0900

    웹명함 전면 교체

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
-function copyEmail(event){
+document.addEventListener("DOMContentLoaded", () => {
 
-    event.preventDefault();
+    const emailBtn =
+        document.getElementById("email-copy");
+
+    emailBtn.addEventListener(
+        "click",
+        copyEmail
+    );
+
+});
+
+
+function copyEmail(){
 
     navigator.clipboard.writeText(
         "seogs1004@naver.com"
-    ).then(() => {
+    )
+    .then(() => {
 
-        alert("이메일이 클립보드에 복사되었습니다.");
+        alert(
+            "이메일이 클립보드에 복사되었습니다."
+        );
 
-    }).catch(() => {
+    })
+    .catch((err) => {
 
-        alert("복사에 실패했습니다.");
+        console.error(err);
+
+        alert(
+            "복사에 실패했습니다."
+        );
 
     });
+
 }
 
 
@@ -29,21 +49,23 @@ END:VCARD`;
 
 const blob = new Blob(
     [vcard],
-    {type:'text/vcard'}
+    {
+        type:'text/vcard'
+    }
 );
 
-const url = URL.createObjectURL(blob);
+const url =
+URL.createObjectURL(blob);
 
-const a = document.createElement('a');
+const a =
+document.createElement('a');
 
 a.href = url;
-a.download = 'SEO_JISU.vcf';
 
-document.body.appendChild(a);
+a.download =
+'SEO_JISU.vcf';
 
 a.click();
-
-document.body.removeChild(a);
 
 URL.revokeObjectURL(url);
 
